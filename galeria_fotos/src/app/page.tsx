2e8d11ec9457ef4e7370956a2bcfa6f1d7592bc1
boma026 <arthurboma@yahoo.com.br>
2025-04-10 14:21:45 -0300

    10/04/25

--- galeria_fotos/src/app/page.tsx
+++ galeria_fotos/src/app/page.tsx
@@ -1,12 +1,49 @@
-import Image from "next/image";
+"use client"
+import { Modal } from "@/components/Modal";
+import { PhotoItem } from "@/components/PhotoItem";
+import { photoList } from "@/data/photoList";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [showModal, setShowModal] = useState(false);
+  const [imageOfModal, setImageOfModal] = useState("");
+  
+  const openModal = (id:number) => {
+    const photo = photoList.find(item =>{
+      if(item.id === id)
+        return true;
+      else{
+        return false;
+      }
+    })
+    if (photo) {
+      setImageOfModal(photo.url);
+      setShowModal(true)
+    }
+  }
+
+  const closeModal = () => {
+    setShowModal(false)
+  }
+
   return (
  
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1>Fotos intergalaticas</h1>
+    <div className="mx-2">
+     <h1 className="text-center text-3xl font-bold my-10">Fotos intergalaticas</h1>
 
-     <section className="container max-w-5xl mx-auto"></section>
+     <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {photoList.map(item => (
+        <PhotoItem
+          key={item.id}
+          photo={item}
+          onClick={() => openModal(item.id)}
+        />
+      ))} 
+     </section>
+     {showModal &&
+     <Modal image={imageOfModal} closeModal={closeModal}/>
+    }
     </div>
   );
 }
