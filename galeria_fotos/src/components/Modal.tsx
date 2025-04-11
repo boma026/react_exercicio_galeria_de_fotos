f321c1f7c2fea89fec8a1996092508852c896056
boma026 <arthurboma@yahoo.com.br>
2025-04-11 14:03:50 -0300

    exercicio finalizado

--- galeria_fotos/src/components/Modal.tsx
+++ galeria_fotos/src/components/Modal.tsx
@@ -6,10 +6,10 @@ type Props = {
 export const Modal = ({image, closeModal}: Props) => {
     return(
         <>
-            <div className="fixed left-0 top-0 right-0 bottom-0 flex justify-center items-center ">
+            <div onClick={closeModal} className="fixed left-0 top-0 right-0 bottom-0 flex justify-center items-center ">
                 <img src={`/assets/${image}`} alt="" className="max-w-screen max-h-screen"/>
             </div>
-            <div className="fixed top-5 right-5 w-10 h-10 cursos-pointer text-white text-5xl">
+            <div onClick={closeModal} className="fixed top-5 right-5 w-10 h-10 cursor-pointer text-white text-5xl">
                 x
             </div>
         </>
