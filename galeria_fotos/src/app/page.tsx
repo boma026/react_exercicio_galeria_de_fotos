f3301cb60886f6fa102130697eac67d9b803e19c
boma026 <arthurboma@yahoo.com.br>
2025-06-11 07:09:44 -0300

    exercicio finalizado

--- galeria_fotos/src/app/page.tsx
+++ galeria_fotos/src/app/page.tsx
@@ -1,13 +1,40 @@
+"use client"
+import { useState } from "react";
 import { photoData } from "./data/photoData";
+import { Modal } from "./components/Modal";
 
 export default function page () {
+
+  const [showModal, setShowModal] = useState(false);
+  const [modalImage, setModalImage] = useState("");
+
+  const openModal = (id: number) => {
+    const photo = photoData.find((item) => item.id === id)
+    if (photo){
+      setModalImage(photo.image);
+      setShowModal(true);
+    }  
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+  
   return (
-    <div className="w-screen h-screen bg-black text-white font-bold text-3xl flex items-center flex-col">
-      <p className="pt-8 pb-8">Fotos intergaláticas</p>
-      <div className="grid grid-cols-3 gap-4">
+    <div className="bg-black min-h-screen w-full">
+      <p className="text-center text-3xl font-bold text-white">Fotos intergaláticas</p>
+      <div className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {photoData.map((item) =>
-          <img key={item.id} src={item.image}/>)}
+          <img 
+            key={item.id} 
+            src={item.image} 
+            onClick={() => openModal(item.id)}
+            className="cursor-pointer"
+          />)}
       </div>
+{showModal &&
+      <Modal onClose={closeModal} image={modalImage} />
+    }
     </div>
   )
 }
