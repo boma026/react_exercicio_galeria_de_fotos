a7c5ebdc4adfaa54e7282647ad740308e5cb51d6
boma <arthurboma@yahoo.com.br>
2025-06-12 10:19:22 -0300

    feito

--- exercicios/src/app/components/Quiz.tsx
+++ exercicios/src/app/components/Quiz.tsx
@@ -5,14 +5,14 @@ import { questionsData } from "../data/questionsData"
 
 export const Quiz = () => {
     
-    const [checkOption, setScheckOption] = useState<boolean>();
+    const [checkOption, setCheckedOption] = useState<number>();
+    const [isCorrect, setIsCorrect] = useState<boolean>();
     const [buttonClicked, setButtonClicked] = useState<number>();
-
-    const handleCheckButton = (answer: boolean, index: number) => {
-        
-        setScheckOption(answer);
-       setButtonClicked(index);
-
+0
+    const handleCheckButton = ( index: number, itemAnswer: number) => {
+        setCheckedOption(itemAnswer);
+        setButtonClicked(index);
+        setIsCorrect(index === itemAnswer);
     }
 
     
@@ -24,10 +24,11 @@ export const Quiz = () => {
             <div key={item.id} className="flex flex-col" >
 
                 <h1 className="font-bold text-2xl mb-8 pl-4">{item.id}. {item.title}</h1>
+                
 
-                {item.options.map((option, index) =>
-                    <button key={index} onClick={() => handleCheckButton(option.answer, index)} className={`cursor-pointer b-4 ml-3 pl-3 mb-4 flex border border-gray-300 rounded mr-3 bg-blue-200 ${
-                        index === buttonClicked? (checkOption? "bg-green-300": "bg-red-400"): "bg-blue-300"}`}>{option.text}</button>)}
+                {item.options.map((alt, index) =>
+                    <button key={index} onClick={() => handleCheckButton(index, item.answer)} className={`cursor-pointer b-4 ml-3 pl-3 mb-4 flex border border-gray-300 rounded mr-3 bg-blue-200 ${
+                        index === buttonClicked? (isCorrect? "bg-green-300": "bg-red-400"): "bg-blue-300"}`}>{alt}</button>)}
             
             </div>
             )}
--- exercicios/src/app/types/QuestionQuiz.ts
+++ exercicios/src/app/types/QuestionQuiz.ts
@@ -1,5 +1,3 @@
-import { Options } from "./Options";
-
 export type QuestionQuiz = {
     id:number,
     title:string,
