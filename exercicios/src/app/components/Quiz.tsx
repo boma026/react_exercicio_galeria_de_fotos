036c3d721f983055075771cc0b56bdddbf35700d
boma <arthurboma@yahoo.com.br>
2025-06-13 06:56:45 -0300

    feito

--- exercicios/src/app/components/Quiz.tsx
+++ exercicios/src/app/components/Quiz.tsx
@@ -4,36 +4,44 @@ import { useState } from "react"
 import { questionsData } from "../data/questionsData"
 
 export const Quiz = () => {
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+    const [selectedOption, setSelectedOption] = useState<number | null>(null);
     
-    const [checkOption, setCheckedOption] = useState<number>();
-    const [isCorrect, setIsCorrect] = useState<boolean>();
-    const [buttonClicked, setButtonClicked] = useState<number>();
-0
-    const handleCheckButton = ( index: number, itemAnswer: number) => {
-        setCheckedOption(itemAnswer);
-        setButtonClicked(index);
-        setIsCorrect(index === itemAnswer);
+    const currentQuestion = questionsData[currentQuestionIndex];
+
+    const handleCheckButton = (index: number, itemAnswer: number) => {
+        if (selectedOption === null) 
+            setSelectedOption(index);
+        
+        setTimeout(() => {
+            setSelectedOption(null);
+            setCurrentQuestionIndex((prev) => prev+1);
+        }, 2000)
+
     }
 
-    
     return (
         <div className="text-black">
-            {questionsData.map((item) => {
-            return(    
-
-            <div key={item.id} className="flex flex-col" >
-
-                <h1 className="font-bold text-2xl mb-8 pl-4">{item.id}. {item.title}</h1>
+            {currentQuestion ? (
+                <div key={currentQuestion.id} className="flex flex-col">
+                <h1 className="font-bold text-2xl mb-8 pl-4">{currentQuestion.id}. {currentQuestion.title}</h1>
                 
-
-                {item.options.map((alt, index) =>
-                    <button key={index} onClick={() => handleCheckButton(index, item.answer)} className={`cursor-pointer b-4 ml-3 pl-3 mb-4 flex border border-gray-300 rounded mr-3 bg-blue-200 ${
-                        index === buttonClicked? (isCorrect? "bg-green-300": "bg-red-400"): "bg-blue-300"}`}>{alt}</button>)}
-            
+                {currentQuestion.options.map((alt, index) => (
+                    <button 
+                        key={index}
+                        onClick={() => handleCheckButton(index, currentQuestion.answer)}
+                        className={`b-4 ml-3 pl-3 mb-4 flex border border-gray-300 rounded mr-3 bg-blue-200 
+                            ${selectedOption !== null ? "cursor-auto" : "cursor-pointer hover:opacity-60"} 
+                            ${selectedOption !== null && selectedOption === currentQuestion.answer && selectedOption === index && "bg-green-300"}
+                            ${selectedOption !== null && selectedOption !== currentQuestion.answer && selectedOption === index && "bg-red-200"}`}
+                    >
+                        {alt}
+                    </button>
+                ))}
             </div>
-            )}
-            )}
-            
-        </div>
+        ) : (
+            <h2 className="text-xl font-bold">Quiz finalizado</h2>
+        )}
+     </div>
     )
 }
--- exercicios/src/app/data/questionsData.ts
+++ exercicios/src/app/data/questionsData.ts
@@ -4,5 +4,12 @@ export const questionsData:QuestionQuiz[] = [{
     id:1,
     title:"Qual time venceu mais campeonatos da NBA até 2024?",
     options: ["Chicago Bulls", "Golden State Warriors", "Los Angeles Lakers", "Miami Heat"],
-    answer: 2}
+    answer: 2
+    },
+    {
+    id:2,
+    title: "Quem é o maior pontuador da história da NBA (temporada regular)?",
+    options:["LeBron James" , "Kareem Abdul-Jabbar", "Karl Malone", "Michael Jordan"],
+    answer:0
+    }
 ]
